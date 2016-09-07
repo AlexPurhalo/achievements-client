9d8a3aad8c6bb71f4894dfa520056cdd3182eb3e
alexpurhalo <alexpurhalo@gmail.com>
2016-09-07 04:38:24 -0400

    header-existing-test

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,6 +1,18 @@
+// Test helpers import
 import { renderComponent, expect } from '../test_helper';
+
+// Components import
 import App from '../../src/components/app';
 
+// Test cases
 describe('App', () => {
+	let component;
 
+	beforeEach(() => {
+		component = renderComponent(App);
+	});
+
+	it('contains a header', () => {
+		expect(component.find('.header')).to.exist;
+	})
 });
