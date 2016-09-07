e7043eb39a5c15f3906f2c55ea7ced2cfdf9a5ca
alexpurhalo <alexpurhalo@gmail.com>
2016-09-07 17:01:58 -0400

    links-changing-with-authenticated-boolean-changing

--- README.md
+++ README.md
@@ -6,7 +6,5 @@
 </ul>
 <h3>Progress</h3>
 <ul>
-	<li>Click on Sign Out link and you gonna redirected to root path</li>
-	<li>Your JWT token should be deleted from localStorage</li>
-	<li>Authentication authenticated boolean should be changed to false, checkout console</li>
+	<li>See how changes links when you signed in and signed out</li>
 </ul>
--- src/components/header.js
+++ src/components/header.js
@@ -11,44 +11,57 @@ class Header extends Component {
 	}
 
 	renderLinks() {
-		return [
-			<li className="nav-item" key={1}>
-				<IndexLink
-					to="/"
-					className="nav-link"
-					activeClassName="active">
-					Persons
-				</IndexLink>
-			</li>,
-			<li className="nav-item" key={2}>
-				<Link
-					to="persons/new"
-					className="nav-link"
-					activeClassName="active">
-					Create Account
-				</Link>
-			</li>,
-			<li className="nav-item" key={3}>
-				<Link
-					to="sign_in"
-					className="nav-link">
-					Sign In
-				</Link>
-			</li>,
-			<li className="nav-item" key={4}>
-				<Link
-					onClick={this.handleClick.bind(this)}
-					to="/"
-					className="nav-link">
-					Sign Out
-				</Link>
-			</li>
-		]
+		if (!this.props.authenticated) {
+			return [
+				<li className="nav-item" key={1}>
+					<IndexLink
+						to="/"
+						className="nav-link"
+						activeClassName="active">
+						Persons
+					</IndexLink>
+				</li>,
+				<li className="nav-item" key={2}>
+					<Link
+						to="persons/new"
+						className="nav-link"
+						activeClassName="active">
+						Create Account
+					</Link>
+				</li>,
+				<li className="nav-item" key={3}>
+					<Link
+						to="sign_in"
+						className="nav-link">
+						Sign In
+					</Link>
+				</li>
+			]
+		} else {
+			return [
+				<li className="nav-item" key={1}>
+					<IndexLink
+						to="/"
+						className="nav-link"
+						activeClassName="active">
+						Persons
+					</IndexLink>
+				</li>,
+				<li className="nav-item" key={2}>
+					<Link
+						onClick={this.handleClick.bind(this)}
+						to="/"
+						className="nav-link">
+						Sign Out
+					</Link>
+				</li>
+			]
+		}
 	}
 
 	// JSX rendering
 	render() {
-		console.log(`authenticated boolean: ${this.props.authenticated}`);
+		//console.log(`authenticated boolean: ${this.props.authenticated}`);
 		return (
 			<div className="header">
 				<nav className="navbar navbar-light">
