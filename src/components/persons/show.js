4c28adc95441bbceaf052b29c521fae74920906a
alexpurhalo <alexpurhalo@gmail.com>
2016-09-23 15:13:23 -0400

    single-person-page-styling

--- src/components/persons/show.js
+++ src/components/persons/show.js
@@ -25,48 +25,81 @@ class Person extends Component {
 			<div className="single-user-page">
 				<div className="row">
 					<div className="col-md-12">
-						<h1>Ruby on Rails + React JS developer</h1>
+						<div className="profile-main-info">
+							<h1 className="person-profile">Full-Stack Developer</h1>
+							<h2 className="person-skills">(Ruby on Rails + ReactJS)</h2>
+						</div>
 					</div>
 				</div>
 				<div className="row">
 					<div className="col-md-6">
 						<div className="thumbnail">
-							<img src={`http://localhost:3000/${imageUrl}`} width="300px" height="300px" alt="Image" />
+							<img src={`http://localhost:3000/${imageUrl}`} alt="Image" />
 						</div>
 					</div>
 					<div className="col-md-6">
-						<h2>Alexnder Purhalo</h2>
-						<h3>alexpurhalo@gmail.com</h3>
-						<h4>+38 095 220 78 66</h4>
-						<h4>alexpurhalo</h4>
+						<div className="person-info">
+							<h2>Alexnder Purhalo</h2>
+							<h3>
+								Kiev, 19 years
+							</h3>
+							<h4 className="person-phone">
+								<img
+									src="http://www.clipartbest.com/cliparts/dT6/nXB/dT6nXBGT9.png"
+									alt="phone-icon"
+									className="phone-icon"/>
+								+38 095 220 78 66
+							</h4>
+							<h4 className="person-email">
+								<img
+									src="http://www.crescentinteractive.com/wp-content/uploads/2014/10/Email-Icon-Image.png"
+									alt="email"
+									className="email-icon" />
+								alexpurhalo@gmail.com
+							</h4>
+							<h4 className="person-skype">
+								<img
+									src="https://upload.wikimedia.org/wikipedia/commons/e/ec/Skype-icon-new.png"
+									alt="skype-icon"
+									className="skype-icon"/>
+								alexpurhalo
+								<img
+									src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-256.png"
+									alt="skype-icon"
+									className="github-icon"/>
+								<img
+									src="http://marisasanfilippo.com/wp-content/uploads/2015/07/Linkedin_circle.svg_.png"
+									alt="skype-icon"
+									className="linkedin-icon"/>
+							</h4>
+						</div>
 					</div>
 				</div>
-				{/*<div>{imageUrl ? (<img src={imageUrl} width="300px" height="300px"/>) : (<div>No photo</div>)}</div><br/>*/}
-				{/*User: <strong>{this.props.person.email}</strong><br/>*/}
-				{/*Name: <strong>{this.props.person.name}</strong><br/>*/}
-				{/*<h2>{this.props.person.profile}</h2*/}
-				{/*><br/>*/}
-				{/*Age: <strong>{this.props.person.age}</strong><br/>*/}
-				{/*Skype: <strong>{this.props.person.skype}</strong><br/>*/}
-				{/*Mobile: <strong>{this.props.person.mobile}</strong><br/>*/}
-
-				{/*<Achievements*/}
-				{/*achievements={this.props.achievements}*/}
-				{/*access_token={this.props.person.access_token}*/}
-				{/*personId={this.props.person.id} />*/}
-
-				<div>
-					{
-						this.props.person.access_token === token
-							?
-							(
-								<div><Link to={`/persons/${this.props.params.id}/edit`}>You can</Link></div>
-							)
-							:
-							(
-								<div>Sorry, you can't do this</div>
-							)
-					}
+				<div className="person-achievements">
+					<div className="row">
+						<div className="col-md-12">
+							<h2 className="person-achievements-title">Summary of Qualifications</h2>
+						</div>
+						<div className="row">
+							<div className="col-md-12">
+								<ul className="achievement-list">
+									<li className="achievements-list-item">Self education progress: one relised product and over than 30 pet projects (1,5 years)</li>
+									<li className="achievements-list-item">Commercial experiance: manual QA engeneer; (0,5 years)</li>
+									<li className="achievements-list-item">Basic: Ruby (0,5+ years), JavaScript (0,5+ years), HTML & CSS (1+ year)</li>
+									<li className="achievements-list-item">Technologies: Ruby on Rails, ReactJS, JQuer, AngularJS, NodeJS</li>
+									<li className="achievements-list-item">Databases: PostgreSQL, MySQL,  MongoDB</li>
+									<li className="achievements-list-item">Testing: Rspec, Mocha</li>
+									<li className="achievements-list-item">Code structure: HAML, ES6, SASS</li>
+									<li className="achievements-list-item">OS: prefere Linux</li>
+									<li className="achievements-list-item">VCS: love to write clear commit history with Git</li>
+									<li className="achievements-list-item">Services: Heroku, Amazon</li>
+									<li className="achievements-list-item">English: use for reading, watching technical things, messaging, speaking meetings</li>
+									<li className="achievements-list-item">Computer science: understanding of Data Structure and Algorithms</li>
+									<li className="achievements-list-item">Personal qualites: fast learner, team player</li>
+								</ul>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		);
