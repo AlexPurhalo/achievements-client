3888f027897f37f336b7cc71341b49048240b1dc
alexpurhalo <alexpurhalo@gmail.com>
2016-09-27 05:14:33 -0400

    skills-section-refactor

--- src/components/persons/show/skills.js
+++ src/components/persons/show/skills.js
@@ -40,70 +40,77 @@ class Skills extends Component {
 		}
 	}
 
+	// Skills form render
+	addSkillFormRender() {
+		return (
+			<form
+				onSubmit={this.handleSubmitSkill}
+				className="form-inline skills-adding-section">
+				<input
+					onChange={this.handleChangeSkill}
+					value={this.state.body}
+					type="text"
+					placeholder="Enter skill"
+					className="form-control add-skill-input"/>
+				<button
+					type="submit"
+					className="btn btn-default">Add
+				</button>
+			</form>
+		);
+	}
+
+
+	// Skills List Render
+	firstSkillRender() {
+		return (
+			<li className="person-skill">
+				{this.props.skills[0].body}&nbsp;
+				{this.deleteSkillFunc(this.props.skills[0].id)}
+			</li>
+		);
+	}
+
+	lastSkillRender() {
+		if (this.props.skills.length > 1) {
+			return (
+				<li className="person-skill">
+					&nbsp;+ {this.props.skills[this.props.skills.length - 1].body}
+					{this.deleteSkillFunc(this.props.skills[this.props.skills.length - 1].id) }
+				</li>
+			)
+		}
+	}
+
+	skillsListRender() {
+		return (
+			<ul className="skills-list">
+				<li className="person-skill">(</li>
+				{this.firstSkillRender()}
+				{
+					this.props.skills.slice(1, this.props.skills.length - 1)
+						.map(skill => {
+							return (
+								<li className="person-skill" key={skill.id}>
+									&nbsp;+ {skill.body}
+									{this.deleteSkillFunc(skill.id)}
+								</li>
+							)
+						})
+				}
+				{this.lastSkillRender()}
+				<li className="person-skill">)</li>
+			</ul>
+		);
+	}
+
 	render() {
 		const token = localStorage.getItem('token');
 
 		return (
 			<h2 className="person-skills">
-				{
-					this.props.access_token  === token
-						?
-						(
-							<form
-								onSubmit={this.handleSubmitSkill}
-								className="form-inline skills-adding-section">
-								<input
-									onChange={this.handleChangeSkill}
-									value={this.state.body}
-									type="text"
-									placeholder="Enter skill"
-									className="form-control add-skill-input"/>
-								<button
-									type="submit"
-									className="btn btn-default">Add
-								</button>
-							</form>
-						)
-						:
-						null
-				}
-				{
-					this.props.skills.length > 0
-						?
-						(
-							<ul className="skills-list">
-								<li className="person-skill">(</li>
-								<li className="person-skill">
-									{this.props.skills[0].body}&nbsp;
-									{this.deleteSkillFunc(this.props.skills[0].id)}
-								</li>
-								{this.props.skills.slice(1, this.props.skills.length - 1)
-									.map(skill => {
-										return (
-											<li className="person-skill" key={skill.id}>
-												&nbsp;+ {skill.body}
-												{this.deleteSkillFunc(skill.id)}
-											</li>
-										)
-									})
-								}
-								{
-									this.props.skills.length > 1
-										?
-										(
-											<li className="person-skill">
-												&nbsp;+ {this.props.skills[this.props.skills.length - 1].body}
-												{this.deleteSkillFunc(this.props.skills[this.props.skills.length - 1].id) }
-											</li>
-										)
-										: null
-								}
-								<li className="person-skill">)</li>
-							</ul>
-						)
-						:
-						null
-				}
+				{this.props.access_token  === token ? (this.addSkillFormRender()) : null}
+				{ this.props.skills.length > 0 ? (this.skillsListRender()) : null }
 			</h2>
 		);
 	}
